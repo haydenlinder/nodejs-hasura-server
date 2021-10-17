36526b0d639c9ce6cc5e84b5f81c2103a50c145b
haydenlinder <haydenlinder@gmail.com>
2021-10-16 20:40:24 -0700

    Edit

--- src/utils/hasuraGQLQuery.ts
+++ src/utils/hasuraGQLQuery.ts
@@ -1,11 +1,12 @@
-import fetch from 'node-fetch'
+import fetch, { RequestInit } from 'node-fetch'
 import { IncomingHttpHeaders } from 'node:http'
 const { HASURA_GQL_URL, HASURA_GQL_SECRET } = process.env
 
 export default async function hasuraGQLQuery (query: string, variables: object, oldHeaders: IncomingHttpHeaders) {
     // Force { [key: string]: string } to play nice with fetch
-    const headers = JSON.parse(JSON.stringify(oldHeaders))
+    // const headers = JSON.parse(JSON.stringify(oldHeaders))
     // Set admin secret to access gql server
+    const headers: RequestInit["headers"] = {};
     headers['x-hasura-admin-secret'] = HASURA_GQL_SECRET!
     // Fetch response
     const response = await fetch(
